ec6f8ab4e0683e8ef875e17e8fb3a8fd283a3beb
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2017-11-06 13:15:14 +0100

    Basic connection tweeter/watson

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var app = new Vue({
     el: '#app',
     data: {
+        text: [],
         tones: []
     },
     created: function() {
         this.$http.get('http://localhost:4200/')
             .then((data) => {
+                console.log(data)
+                this.text = data.body.text.split('. ')
                 this.tones = data.body.tones
             })   
     }
--- server.js
+++ server.js
@@ -10,30 +10,30 @@ var analyzer = new ToneAnalyzerV3({
   version_date: '2017-09-21'
 });
 
+var T = new Twit({
+    consumer_key: '899sWOGu9LqBsnVksIlmgvOeE',
+    consumer_secret: 'qeLK9cjSHP3tc3JJ1V48NJUEP5QJyTEiNdGmGuFdnyW7qHm8Cl',
+    access_token: '700723572751269888-8lrBXE2xlyjoyMKupr8sFZ6JUExoxqC',
+    access_token_secret: 'OpGGe5VGTYdvUPROaJ2eQuQQBSTldD5dg4besNHUYkss6',
+    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+})
 
 app.get('/', (request, response) => {
     console.log(`${request.method} ${request.path} ${Date()}`)
 
+    response.tones = []
     response.header('Access-Control-Allow-Origin', '*');
 
-    var T = new Twit({
-        consumer_key: '899sWOGu9LqBsnVksIlmgvOeE',
-        consumer_secret: 'qeLK9cjSHP3tc3JJ1V48NJUEP5QJyTEiNdGmGuFdnyW7qHm8Cl',
-        access_token: '700723572751269888-8lrBXE2xlyjoyMKupr8sFZ6JUExoxqC',
-        access_token_secret: 'OpGGe5VGTYdvUPROaJ2eQuQQBSTldD5dg4besNHUYkss6',
-        timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-    })
-
     var result = T.get('search/tweets', {
-        q: 'bitcoin',
+        q: 'ethereum',
         count: 100,
         result_type: 'popular'
     }, (err, data, answer) => {
-
-        analyzer.tone({ text: data.statuses[0].text }, (err, tone) => {
+        var text = data.statuses.map(o => o.text).join('. ')
+        analyzer.tone({ text: text }, (error, tone) => {
             response.send({
-                text: data.statuses[0].text,
-                tones: tone.sentences_tone.reduce((acc, curr) => acc.concat(curr.tones), [])
+                text: text,
+                tones: tone.document_tone.tones
             });
         });
     });
