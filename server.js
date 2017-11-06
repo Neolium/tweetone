aa73697cb4570ae2349d99531ac7560ad2b21e56
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2017-11-06 12:09:16 +0100

    Basic Watson API usage with SDK

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var Twit = require('twit')
 var express = require('express')
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
 
 var app = express()
+
+var analyzer = new ToneAnalyzerV3({
+  username: '1d983e8a-1114-4d70-8e1d-e22342862aad',
+  password: 'jBYGMQ0vHFTA',
+  version_date: '2017-09-21'
+});
+
 
 app.get('/', (request, response) => {
     console.log(`${request.method} ${request.path} ${Date()}`)
@@ -19,8 +27,12 @@ app.get('/', (request, response) => {
         count: 100,
         result_type: 'popular'
     }, (err, data, answer) => {
-        response.send({
-            tweets: data.statuses.map(o => o.text)
+
+        analyzer.tone({ text: data.statuses[0].text }, (err, tone) => {
+            response.send({
+                text: data.statuses[0].text,
+                tones: tone.sentences_tone.reduce((acc, curr) => acc.concat(curr.tones), [])
+            });
         });
     });
 })
